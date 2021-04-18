8ff874339a394da501cbc25cb1fbd8a20ada91a9
Denny1027 <47983284+Denny1027@users.noreply.github.com>
2021-04-18 20:14:46 +0200

    Add files via upload

---
+++ ML/ml_classes/ann.py
@@ -0,0 +1,117 @@
+import tensorflow as tf
+
+
+class Ann:
+
+    def __init__(self, name):
+        self._nn = tf.keras.models.Sequential()
+        self._name = name
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    def create_ann(self):
+        pass
+
+    def save_ann(self):
+        self._nn.save('./ml_classes/ml_modules/'+self._name)
+
+    def load_ann(self):
+        try:
+            self._nn = tf.keras.models.load_model('./ml_classes/ml_modules/'+self._name)
+            print(self._name, " caricato!")
+        except (IOError, ImportError):
+            print("Errore durante il caricamento di ", self._name, ". Modello non caricato!")
+
+    def train(self, x, y, batch_size=32, epochs=100):
+        try:
+            self._nn.fit(x, y, batch_size=batch_size, epochs=epochs)
+            print("Training eseguito con successo!")
+        except RuntimeError:
+            print("Training Error: modello non compilato!")
+        except ValueError:
+            print("Training Error: Dati in input vuoti o del formato errato")
+
+    def predict(self, prediction):
+        return self._nn.predict(prediction)
+
+
+class BinaryNNClassificator(Ann):
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def __init__(self, *args, name):
+        super().__init__(name)
+
+        if len(args) < 1:
+            print("Numero di layers insufficienti. ANN vuota. Inserire almeno 1 hidden layer")
+            return
+
+        for arg in args:
+            self._nn.add(tf.keras.layers.Dense(units=arg, activation='relu'))
+
+        self._nn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+        print("ANN creata con successo!")
+        print("dimensione: {} hidden {}; Dimensione output 1".format(len(args), args))
+        self._nn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        print("ANN compilato con successo: LossFunction = 'binary_crossentropy'")
+
+    def create_ann(self, *args):
+        if len(args) < 1:
+            print("Numero di layers insufficienti. ANN vuota. Inserire almeno 1 hidden layer")
+            return False
+
+        for arg in args:
+            self._nn.add(tf.keras.layers.Dense(units=arg, activation='relu'))
+        self._nn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+        print("ANN creata con successo!")
+        print("dimensione: {} hidden {}; Dimensione output 1".format(len(args), args))
+        self._nn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        print("ANN compilato con successo: LossFunction = 'binary_crossentropy'")
+        return True
+
+
+
+
+class NNClassificator(Ann):
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def __init__(self, *args, name):
+        super().__init__(name)
+
+        if len(args) <= 1:
+            print("Numero di layers insufficienti. ANN vuota. Inserire almeno 1 hidden layer e 1 output layer")
+            return
+
+        for arg in args[:-1]:
+            self._nn.add(tf.keras.layers.Dense(units=arg, activation='relu'))
+        self._nn.add(tf.keras.layers.Dense(units=args[-1], activation='softmax'))
+
+        print("ANN creata con successo!")
+        print("dimensione: {} hidden {}; Dimensione output: {}".format(len(args[:-1]), args[:-1], args[-1]))
+        self._nn.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        print("ANN compilato con successo: LossFunction = 'categorical_crossentropy'")
+
+    def create_ann(self, *args):
+        if len(args) <= 1:
+            print("Numero di layers insufficienti. ANN vuota. Inserire almeno 1 hidden layer e 1 output layer")
+            return False
+
+        for arg in args[:-1]:
+            self._nn.add(tf.keras.layers.Dense(units=arg, activation='relu'))
+
+        self._nn.add(tf.keras.layers.Dense(units=args[-1], activation='softmax'))
+
+        print("ANN creata con successo!")
+        print("dimensione: {} hidden {}; Dimensione output: {}".format(len(args[:-1]), args[:-1], args[-1]))
+        self._nn.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        print("ANN compilato con successo: LossFunction = 'categorical_crossentropy'")
+        return True
